bf2cc66d2b0155acad7ac5cb2594cd89e2c30530
Manas12Singh <rd511323@gmail.com>
2023-08-16 16:02:58 +0530

    first update

---
+++ c.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, avg;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    avg = (a + b) / 2;
+    cout << "Average is: " << avg << endl;
+    return 0;
+}
