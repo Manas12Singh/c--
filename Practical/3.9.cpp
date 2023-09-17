4e5edd734b85f35e18e4a6eb84c2b1d28f2f348e
Manas12Singh <rd511323@gmail.com>
2023-09-18 00:17:19 +0530

    done

--- Practical/3.9.cpp
+++ Practical/3.9.cpp
@@ -19,17 +19,19 @@ int main()
     }
     for (int i = 0; i < n; i++)
     {
-        charges[i] = 50;
+        charges[i] = 0;
         if (units[i] > 300)
             charges[i] += (units[i] - 300) * 0.9 + 220;
         else if (units[i] > 100)
             charges[i] += (units[i] - 100) * 0.8 + 60;
         else
             charges[i] += units[i] * 0.6;
+        if (charges[i] < 50)
+            charges[i] = 50;
         if (charges[i] > 300)
             charges[i] *= 1.15;
     }
     cout << setw(10) << "Name" << setw(20) << "Charges(In Rs.)" << endl;
     for (int i = 0; i < n; i++)
-        cout << setw(10) << name[i] << setw(15) << charges[i] << endl;
+        cout << setw(10) << name[i] << setw(20) << charges[i] << endl;
 }
--- Practical/4.10.cpp
+++ Practical/4.10.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    
+    int n;
+    cout << "Enter the number of strings: ";
+    cin >> n;
+    string arr[n];
+    cout << "Enter the strings:" << endl;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    for (int i = n - 1; i >= 0; i--)
+        for (int j = 0; j < i; j++)
+            if (arr[j] > arr[j + 1])
+            {
+                string t = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = t;
+            }
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 }
